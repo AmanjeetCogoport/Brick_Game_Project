839f2d08f44a7388a68706a758437a84f59fd438
= <=>
2022-07-02 02:12:13 +0530

    added basic functionality for the game like generating random color and random number

--- sketch.js
+++ sketch.js
@@ -13,3 +13,29 @@ let direction = "";
 let score = 0, live = 3;
 let possx=400, possy=30;
 
+// added score and live functionality.
+// show scores and live
+function show_score(){
+    fill("white");
+    textSize(20)
+    text("Score : " + score, possx, possy)
+    text("Live : " + live, possx + 120, possy)
+  }
+
+
+// will create random color in rgb.
+function random_color() {
+    color = `rgb(${parseInt(Math.random() * 254) + 1}, ${
+      parseInt(Math.random() * 254) + 1
+    }, ${parseInt(Math.random() * 254) + 1})`;
+    return color;
+  }
+  
+  
+  // will create random number beetween 2 given number.
+  function random_num(x, y) {
+    let number = parseInt(x + Math.random() * (y - x));
+    return number;
+  }
+
+
